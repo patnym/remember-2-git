73c8763d372791ed1b67e185e4b5568afff3d4fd
Patrik Nyman <patrik911217@gmail.com>
2018-02-18 21:58:00 +0100

    Mid commit to  not be a hypocrite * Correctly detects if its a git repo or not * Creates 2 manual triggers that pops after X amount of changes * Removed the FS dependency as VScode could detect git repos

--- src/extension.ts
+++ src/extension.ts
@@ -1,54 +1,137 @@
-'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as fs from 'fs';
+import { exec } from 'child_process';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    //Verify git is installed
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "r2g" is now sadsaactive!');
+
+    //Retrieve configs
+    let settings = vscode.workspace.getConfiguration('r2g');
+    
+    const triggers: ChangeTrigger[] = [];
+    //Create info level trigger
+    triggers.push(
+        new ChangeTrigger(settings.get<number>("reminderInfoLevel"), (nrChanges: number) => {
+            vscode.window.showInformationMessage('Hey! You\'ave currently made ' + nrChanges +
+            ' changes to your repo. Think about commiting your current changes!');
+        })
+    );
+    //Create warning level trigger
+    triggers.push(
+        new ChangeTrigger(settings.get<number>("reminderWarningLevel"), (nrChanges: number) => {
+            vscode.window.showWarningMessage('Hey! You\'ave currently made ' + nrChanges +
+            ' changes to your repo. Think about your team, commit these changes now!');
+        })
+    );
 
     //Search the root dir to find out if this is a git repo or not
     let wsf: vscode.WorkspaceFolder[] | undefined = vscode.workspace.workspaceFolders;
 
     if(wsf !== undefined) {
-        fs.exists(wsf[0].uri.fsPath + '/.git', (exists) => {
-            if(exists) {
-                vscode.window.showInformationMessage('Hey! This is a git repo, do you want to activate g2r?');
-            }
+        const wdPath: string = wsf[0].uri.fsPath;
+        vscode.window.showInformationMessage('Hey! This is a git repo, do you want to activate g2r?');
+
+        //Start watch task
+        let disposable: vscode.Disposable = vscode.workspace.onDidSaveTextDocument((e: vscode.TextDocument) => {
+            console.log('saved..');
+            
+            exec('git -C ' + wdPath + '  diff --shortstat',
+                (err: Error, stdout: string, stderr: string) => {
+                if(!err) {
+                    console.log(stdout);
+                    let nrChanges: number = parseShortStatString(stdout);
+
+                    if(nrChanges) {
+                        vscode.window.showInformationMessage('Hey! You\'ave currently made ' + nrChanges +
+                                ' changes to your repo. Think about commiting your current changes!');
+                    }
+                } else {
+                    console.log("somthing went wrong", err, stderr);
+                }
+            });
         });
+
+        context.subscriptions.push(disposable);
     }
-
-    // let r: Thenable<any[]> = vscode.workspace.findFiles(".git");
-    // r.then( (value: any[]) => {
-    //     if(value.length > 0) {
-    //         vscode.window.showInformationMessage('This is a git repo!');
-    //         console.log('gIt repo');
-    //     }
-    //     console.log('blaa');
-    // }, () => {
-    //     console.log('not git repo');
-    //     vscode.window.showInformationMessage('Not a git repo');
-    // });
-    //Register to typing event and react if we reach a specific threshold
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    // let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-    //     // The code you place here will be executed every time your command is executed
-
-    //     // Display a message box to the user
-    //     vscode.window.showInformationMessage('Hello World!');
-    // });
-
-    // context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
+}
+
+/** 
+ * Parse a git shortstat message and returns a number that represents total changes
+*/
+function parseShortStatString(shortStats: string) : number {
+    console.log(shortStats);    
+    let regex: RegExp = /(\d.)/g; //new RegExp('(\d.)', '/g');
+    let results: RegExpExecArray | null;
+    let changes: number = 0;
+    //Expect to go 3 laps
+    let verifier: number = 3;
+    while(((results = regex.exec(shortStats)) !== null) 
+            || verifier > 0) {             
+        //Skip first result
+        if(verifier === 3) {
+            //no-op
+        } else if(results[0]) {
+            changes += Number(results[0]);
+        }
+        verifier--;
+    }
+    return changes;
+}
+
+/** 
+ * Represents a trigger that reacts to the change nr
+ * Just makes turning triggers off a bit cleaner and adds future support
+ * for eventual custom amount of triggers
+*/
+class ChangeTrigger {
+    triggerLevel: number;
+    triggerFunc: Function;
+    triggerActive: boolean = true;
+    triggerTimeout: number = Date.now();
+
+    constructor(triggerLevel: number, triggerFunc: Function) {
+        this.triggerLevel = triggerLevel;
+        this.triggerFunc = triggerFunc;
+    }
+
+    /**
+     * Verifies this trigger by the number of changes, will trigger a message if changes > triggerLevel.
+     * @param changeNumber number of changes
+     * @param activateTriggerIfFalse if true we activate this trigger again if verify returns false (aka change number has gone down again)
+     */
+    verify(changeNumber: number, activateTriggerIfFalse: boolean) {
+        if(this.triggerActive &&
+            changeNumber > this.triggerLevel && 
+            Date.now() > this.triggerTimeout) {
+            this.triggerFunc(changeNumber);
+
+            //Set trigger timeout, this just pervents 10 triggers in a row if som1 is a bit  spastic
+            //Currently set to 5 minutes
+            this.setCustomTimeout(300);
+
+            //Deactivate trigger
+            this.triggerActive = false;
+        } else if(changeNumber < this.triggerLevel) {
+            this.triggerActive = activateTriggerIfFalse;
+        }
+    }
+
+    /**
+     * Set a custom timeout for this trigger
+     * @param timeOutInSeconds How long this trigger is deactivated in seconds
+     */
+    setCustomTimeout(timeOutInSeconds: number) {
+        this.triggerTimeout = timeOutInSeconds * 1000;
+    }
+
+
 }
